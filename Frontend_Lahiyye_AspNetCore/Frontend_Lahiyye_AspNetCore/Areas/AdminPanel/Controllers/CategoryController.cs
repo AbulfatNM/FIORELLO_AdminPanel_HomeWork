93ca446c16e10f5c19fcf692c0bf65d289e397e3
Abulfat <abulfatnm>
2020-06-23 15:53:02 +0400

    category CRUD

--- Frontend_Lahiyye_AspNetCore/Frontend_Lahiyye_AspNetCore/Areas/AdminPanel/Controllers/CategoryController.cs
+++ Frontend_Lahiyye_AspNetCore/Frontend_Lahiyye_AspNetCore/Areas/AdminPanel/Controllers/CategoryController.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Frontend_Lahiyye_AspNetCore.DAL;
 using Frontend_Lahiyye_AspNetCore.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Frontend_Lahiyye_AspNetCore.Areas.AdminPanel.Controllers
 {
@@ -14,23 +13,23 @@ namespace Frontend_Lahiyye_AspNetCore.Areas.AdminPanel.Controllers
         private readonly AppDbContext _db;
         public CategoryController(AppDbContext db)
         {
-            _db = db;
+            _db=db;
         }
         [Area("AdminPanel")]
         public IActionResult Index()
         {
-
+           
             return View(_db.Categories);
         }
         [Area("AdminPanel")]
-        public async Task<IActionResult> Detail(int? id)
+        public async Task <IActionResult> Detail(int? id)
         {
-            if (id == null)
+            if (id==null)
             {
                 return NotFound();
             }
-            Category category = await _db.Categories.FindAsync(id);
-            if (category == null)
+            Category category =await _db.Categories.FindAsync(id);
+            if (category==null)
             {
                 return NotFound();
             }
@@ -41,93 +40,106 @@ namespace Frontend_Lahiyye_AspNetCore.Areas.AdminPanel.Controllers
         {
             return View();
         }
-        [HttpPost, Area("AdminPanel")]
+        [HttpPost,Area("AdminPanel")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Category category)
+        public async Task <IActionResult> Create(Category category)
         {
             if (!ModelState.IsValid)
             {
                 return View();
             }
-            bool IsValid = _db.Categories.Any(c => c.Name.ToLower() == category.Name.ToLower());
-            if (IsValid)
+            bool isValid = _db.Categories.Any(c => c.Name == category.Name);
+            if (isValid)
             {
-                ModelState.AddModelError("Name", "Bu adli kategory movcuddur");
+                ModelState.AddModelError("Name","Bu adda categoria movcutdur");
                 return View();
             }
-            await _db.Categories.AddAsync(category);
-            await _db.SaveChangesAsync();
+            _db.Categories.Add(category);
+           await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+            //return Content($"{category.Name} {category.Description}");
+        }
+        [Area("AdminPanel")]
+        public async Task <IActionResult> Edit(int? id )
+        {
+            
+            if (id==null)
+            {
+                return NotFound();
+            }
+            Category category =await _db.Categories.FindAsync(id);
+            if (category==null)
+            {
+                return NotFound();
+            }
+            return View(category);
+
+        }
+        [HttpPost,Area("AdminPanel")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int? id, Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Category dbcategory = await _db.Categories.FindAsync(id);
+            Category checkCatgegoey = _db.Categories.FirstOrDefault(c => c.Name.ToLower() == category.Name.ToLower());
+
+            if (checkCatgegoey!=null)
+            {
+                if (dbcategory.Id != checkCatgegoey.Id)
+                {
+                    ModelState.AddModelError("Name", "Bu adli categoria movcutdur !!");
+                    return View();
+                }
+
+            }
+            dbcategory.Name = category.Name;
+            dbcategory.Description = category.Description;
+          await _db.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
 
         }
-        //[Area("AdminPanel")]
-        //public async Task<IActionResult> Edit(int? id)
-        //{
-        //    if (id==null)
-        //    {
-        //        return NotFound();
-        //    }
-        //   Category category=await _db.Categories.FindAsync(id);
-        //    if (category==null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return View(category);
-        //}
-        //public async Task <IActionResult> Edit(int? id,Category category)
-        //{
-        //    if (ModelState.IsValid) return View();
-        //    if (id == null) return NotFound();
-        //    Category dbCategory =await _db.Categories.FindAsync(id);
-        //    Category chekdCategory = _db.Categories.FirstOrDefault(c => c.Name.ToLower() == category.Name.ToLower());
-        //    if (dbCategory.Id!= chekdCategory.Id)
-        //    {
-        //        ModelState.AddModelError("Name", "Bu adli kategory movcuddur");
-        //        return View();
 
-        //    }
-        //    dbCategory.Name = category.Name;
-        //    dbCategory.Description = category.Description;
-        //    await _db.SaveChangesAsync();
-        //    return RedirectToAction(nameof(Index));
-
-
-        //}
         [Area("AdminPanel")]
         public async Task<IActionResult> Delet(int? id)
         {
-            if (id == null)
+            if (id==null)
             {
                 return NotFound();
             }
             Category category = await _db.Categories.FindAsync(id);
-            if (category == null)
+            if (category==null)
             {
                 return NotFound();
-
             }
             return View(category);
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [ActionName("Delet")]
-        [Area("AdminPAnel")]
-        
-        public async Task<IActionResult> DeletPost(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-            Category category = await _db.Categories.FindAsync(id);
-            if (category == null)
-            {
-                return NotFound();
 
+        }
+        [HttpPost,Area("AdminPanel")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delet(int? id,Category category)
+        {
+            if (id==null)
+            {
+                return NotFound();
             }
-            _db.Categories.Remove(category);
+            Category FindcategoryId = await _db.Categories.FindAsync(id);
+            if (category==null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(FindcategoryId);
            await _db.SaveChangesAsync();
+
             return RedirectToAction(nameof(Index));
         }
+
     }
 }
